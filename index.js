bfcedb913117450eba71cb73e9876374b3d57f78
Elsa Helies <elsa.helies@u-psud.fr>
2016-05-09 15:35:22 +0200

    ajout nombre dans index.js

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ window.onload = function() {
         var canvas = document.getElementById('can');
         var context = canvas.getContext('2d');
         var a = [];
+        var nb = 10;
         setInterval(anime, 1000);
        
         function anime() {
             context.fillStyle = "green";
             context.clearRect(0, 0, canvas.width, canvas.height);
-            for (i=0; i<10; i++) {
-                for (j=0; j<10; j++) {
+            for (i=0; i<nb; i++) {
+                for (j=0; j<nb; j++) {
                     if (Math.random() > 0.9) {
                         a[i*10+j] = 1;
                         context.fillStyle = "green";
